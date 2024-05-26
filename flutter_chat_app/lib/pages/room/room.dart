5b5fa5e68252341c618e1c31776f82f62bc2eb7a
pswoonie <swp8111@gmail.com>
2024-05-26 22:29:11 +0900

    [UPDATE] connect socket

--- flutter_chat_app/lib/pages/room/room.dart
+++ flutter_chat_app/lib/pages/room/room.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
-class Room extends StatelessWidget {
+class Room extends StatefulWidget {
   const Room({super.key});
+
+  @override
+  State<Room> createState() => _RoomState();
+}
+
+class _RoomState extends State<Room> {
+  final _channel = WebSocketChannel.connect(Uri.parse('ws://127.0.0.1:3000'));
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +22,24 @@ class Room extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'This is Room',
-              style: Theme.of(context).textTheme.headlineMedium,
+            StreamBuilder(
+              stream: _channel.stream,
+              builder: (context, snapshot) {
+                return Text(
+                  snapshot.hasData ? snapshot.data : '',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
             ),
           ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _channel.sink.close();
+    super.dispose();
   }
 }
